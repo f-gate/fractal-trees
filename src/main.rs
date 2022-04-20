9ee6ee7d0d6ca607d07b6ec2e2ae9e015b4584c4
felix gate <felixgate13@gmail.com>
2022-04-20 14:44:52 +0100

    circle recursion on screen

--- src/main.rs
+++ src/main.rs
@@ -10,25 +10,37 @@ fn main() {
                         curve: 0.8};
 
 
-
     with_2d_graphics(|| {
         let canvas = create_canvas_window("Fractal Tree Generator");
 
         canvas.draw(|gc| {
             gc.center_region(0.0, 0.0, 1000.0, 1000.0);
-
-            gc.new_path();
-            gc.move_to(500.0, 100.0);
-            gc.line_to(500.0, 200.0);
-            gc.fill_color(Color::Rgba(1.0, 0.0, 0.8, 1.0));
-            gc.fill();
+            gc.line_width(5.0);
+            gc.stroke_color(Color::Rgba(1.0, 0.0, 0.8, 1.0));
+           
+            branch(gc, 500.0, 500.0, 1.2, 20);
         });
     
-        branch(&canvas, branch_props);
 
     })
 }
 
+
+fn branch(gc: &mut CanvasGraphicsContext, x : f32 ,y : f32, reduction_factor: f32,mut n: i32) {
+            gc.new_path();
+            gc.circle(x, y, n as f32 * 20.0);
+           // gc.line_to(x , y);
+           // gc.close_path();
+            gc.stroke();
+
+            println!("x: {} y: {}", x, y);
+            if n == 0 {
+              return;
+             }
+            n -= 1;
+            branch(gc, x, y, reduction_factor, n )
+    
+}
 
 
 struct BranchProps {
@@ -40,13 +52,4 @@ struct BranchProps {
 struct Tree {
     branches: Vec<BranchProps>,
     n: i32,
-}
-
-
-fn branch(canvas: &flo_draw::canvas::Canvas, branch: BranchProps) {
-    
-}
-
-fn add(num :i32) {
-
 }
