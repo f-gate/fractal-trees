525f074d20b4aa652e79e2ea8234a1a695df1cfd
felix gate <felixgate13@gmail.com>
2022-04-21 23:21:52 +0100

    tree and branch structs + impl, base height off tree

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,29 @@
+
 use flo_draw::*;
 use flo_draw::canvas::*;
 
 const COLOR: Color = Color::Rgba(1.0, 0.0, 0.8, 1.0);
-const ANGLE: f32 = 10.0;
-const DEPTH: i32 = 11;
+const ANGLE_START: f32 = 10.0;
+const DEPTH: i32 = 15;
 const LINE_WIDTH: f32 = 1.0;
 
 
 fn main() {
-    let branch_props: BranchProps = BranchProps {
-                        color: Color::Rgba(1.0, 0.2, 0.2, 1.0),
-                        length: 500,
-                        curve: 0.8};
-
 
     with_2d_graphics(|| {
         let canvas = create_canvas_window("Fractal Tree Generator");
-
         canvas.draw(|gc| {
-            gc.center_region(0.0, 0.0, 1000.0, 1000.0);
+
+            let mut t = Tree::default();
             gc.line_width(LINE_WIDTH);
             gc.stroke_color(COLOR);
-           
-          //  gc.new_path();
-           // gc.move_to(500.0, 150.0);
-           // gc.line_to(500.0 , 300.0);
-          //  gc.close_path();
-          //  gc.stroke();    
+            t.branch(gc, 500.0, 0.0, 0.0, DEPTH);
+ 
 
-            branch(gc, 500.0, 300.0, 0.0, DEPTH);
+            //make canvas height based off tree generated.
+            gc.center_region(0.0, 0.0, 1000.0, 1000.0);
+
+            gc.canvas_height(t.get_canvas_height() as f32);
         });
     
 
@@ -36,31 +31,66 @@ fn main() {
 }
 
 
-fn branch(gc: &mut CanvasGraphicsContext, x : f32 ,y : f32, angle: f32, n: i32) {
-            let x2 = x + angle.to_radians().sin() * n as f32 * 15.0;
-            let y2 = x - angle.to_radians().cos() * n as f32 * 15.0;
-            gc.new_path();
-            gc.move_to(x, y);
-            gc.line_to(x2 , y2);
-            gc.close_path();
-            gc.stroke();    
 
-            println!("x: {} y: {}", x, y);
-            if n == 0 {
-              return;
-             }
-            branch(gc, x2, y2, angle - ANGLE, n-1);
-            branch(gc, x2, y2, angle + ANGLE, n-1);
-}
-
-
-struct BranchProps {
+#[derive(Debug)]
+pub struct Branch {
     color: Color,
-    length: i32,
-    curve: f32,
+    x: f32,
+    y: f32,
+}
+impl Branch {
+    pub fn new(color: Color, x: f32, y:f32) -> Self {
+         Self {
+            color,
+            x,
+            y,
+         }
+    }
 }
 
-struct Tree {
-    branches: Vec<BranchProps>,
+#[derive(Default, Debug)]
+pub struct Tree {
+    branches: Vec<Branch>,
     n: i32,
+}
+
+impl Tree {
+
+    // get u32 canvas height
+    pub fn get_canvas_height(&self) -> u32 {
+        let mut y_values: Vec<u32> = vec![];
+        self.branches.iter().map(|b|y_values.push(b.y.abs() as u32));
+        println!("{:?}", y_values);
+        match y_values.iter().max() {
+            Some(v) => {
+                println!("{}", v);
+                *v
+            } ,
+            _ => {
+                println!("{}", 0); 
+                0
+            }  
+        }
+    }
+
+    /// Populates the branches of a tree.
+    pub fn branch( &mut self, gc: &mut CanvasGraphicsContext, x : f32 ,y : f32, angle: f32, n: i32) {
+        self.branches.push(Branch::new(COLOR, x, y));
+
+        let x2 = x + angle.to_radians().sin() * n as f32 * 10.0;
+        let y2 = y + angle.to_radians().cos() * n as f32 * 10.0;
+        gc.new_path();
+        gc.move_to(x, y);
+        gc.line_to(x2 , y2);
+        gc.close_path();
+        gc.stroke();    
+
+
+        if n == 0 {
+          return;
+         }
+        self.branch(gc, x2, y2, angle - ANGLE_START, n-1);
+        self.branch(gc, x2, y2, angle + ANGLE_START, n-1);
+}
+
 }
