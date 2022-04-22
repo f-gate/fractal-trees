94f69b2f381e4ca2eae2e3aae4d6fdba7d664a7a
felix gate <felixgate13@gmail.com>
2022-04-22 12:02:08 +0100

    2d coord

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
 use rand::prelude::*;
-//use flo_curves::bezier::*;
+use flo_curves::bezier::*;
 use flo_draw::*;
 use flo_draw::canvas::*;
 use flo_draw::binding::*;
 
 const COLOR: Color = Color::Rgba(1.0, 0.0, 0.8, 1.0);
-const ANGLE_START: f32 = 60.0;
-const DEPTH: i32 = 11;
-const LINE_WIDTH: f32 = 2.5;
+const ANGLE_START: f32 = 25.0;
+const DEPTH: i32 = 13;
+const LINE_WIDTH: f32 = 2.0;
 
 
 
@@ -16,15 +16,18 @@ fn main() {
     with_2d_graphics(|| {
 
         let mut window_properties       = WindowProperties::from(&"Fractal Tree Generator");
-        //window_properties.mouse_pointer = BindRef::from(bind(MousePointer::None));
+        window_properties.mouse_pointer = BindRef::from(bind(MousePointer::None));
+        
         
         let (canvas, events) = create_canvas_window_with_events(window_properties);
+
+
 
         canvas.draw(|gc| {
             let SCR_HEIGHT = 100.0*DEPTH as f32;
             gc.line_width(1.0);
             gc.stroke_color(Color::Rgba(1.0, 1.0, 1.0, 1.0));
-           // gc.draw_rect();
+            gc.draw_rect();
 
 
 
@@ -33,7 +36,7 @@ fn main() {
             gc.canvas_height(SCR_HEIGHT);
             gc.center_region(0.0, 0., SCR_HEIGHT, SCR_HEIGHT);
             
-            draw_tree(gc, SCR_HEIGHT/2.0, SCR_HEIGHT/2.0, 0.0, DEPTH);
+            draw_tree(gc, SCR_HEIGHT/1.5, SCR_HEIGHT/10.0, 0.0, DEPTH);
            
             ()
         });
@@ -44,19 +47,19 @@ fn main() {
     /// Populates the branches of a tree. and draws tree
     pub fn draw_tree(gc: &mut CanvasGraphicsContext, x : f32 ,y : f32, angle: f32, n: i32) {
         let mut rng = thread_rng();
-        let color = Color::Rgba(rng.gen_range(0.1..=1.0), rng.gen_range(0.1..=1.0), rng.gen_range(0.1..=1.0), 1.0);
+        let color = Color::Rgba(rng.gen_range(0.1..=1.0), 0.0, rng.gen_range(0.1..=1.0), 1.0);
 
         let x2 = x + angle.to_radians().sin() * n as f32 * 10.0;
         let y2 = y + angle.to_radians().cos() * n as f32 * 10.0;
 
-       // let bpb = BezierPathBuilder::start(Branch{x: x, y: y});
-        //bpb.line_to(Branch{x: x2, y: y2});
-        //let path = bpb.build();
+        let bpb = BezierPathBuilder::start(Branch{x: x, y: y});
+        bpb.line_to(Branch{x: x2, y: y2});
+        let path = bpb.build()
         //slower than having stroking just once but this allows customisation of branches
+        //gc.stroke_color(color);
         //gc.draw_bzier_curve();
 
         gc.new_path();
-        gc.stroke_color(color);
         gc.move_to(x, y);
         gc.line_to(x2 , y2);
         gc.stroke();
@@ -68,3 +71,19 @@ fn main() {
         draw_tree(gc, x2, y2, angle + ANGLE_START, n-1);
 }
 
+struct Branch  {
+    x: f64,
+    y: f64,
+}
+
+impl Coordinate2D for Branch {
+    fn x(&self) -> f64 {
+        self.x
+    }
+    fn y(&self) -> f64 {
+        self.y
+    }
+    fn coords(&self) -> (f64, f64) {
+        (self.x, self.y)
+     }
+} 
