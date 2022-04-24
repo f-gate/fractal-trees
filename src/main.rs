62c7710eadecfa2b909f1b0b70ce52a92579178b
felix gate <felixgate13@gmail.com>
2022-04-24 13:27:53 +0100

    added clap for cli interface, trying to polish and sign off

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,64 @@
 use rand::prelude::*;
-use flo_curves::bezier::*;
 use flo_draw::*;
 use flo_draw::canvas::*;
 use flo_draw::binding::*;
+use std::*;
+
+use clap::*;
 
 const COLOR: Color = Color::Rgba(1.0, 0.0, 0.8, 1.0);
-const ANGLE_START: f32 = 25.0;
-const DEPTH: i32 = 13;
+const ANGLE_START: f32 = 15.0;
+const DEPTH: i32 = 15;
 const LINE_WIDTH: f32 = 2.0;
 
-
+struct TreeProps {
+    angle_change: f32,
+    depth: i32,
+}
 
 fn main() {
+    let app = Command::new("hello-clap")
+        .version("0.1")
+        .about("draws fractal tree")
+        .author("gatsey");
 
-    with_2d_graphics(|| {
+    let depth = Arg::new("depth")
+        .long("depth") 
+        .default_value("10")
+        .help("factorial for branch iteration, default 10 i32")
+        .required(true);
+    
+    let angle_start = Arg::new("angle")
+        .long("angle") 
+        .default_value("10.0")
+        .help("change angle for branch iteration, default 20.0 float")
+        .required(true);
+
+    // now add in the argument we want to parse
+    let app = app.arg(angle_start);
+    let app = app.arg(depth);
+
+    let matches = app.get_matches();
+
+    // Extract the actual name
+    if let Some(n) = matches.value_of("angle") {
+        //let angle = n.parse::<f32>().expect("angle is not a valid float");
+        println!("{:?}", n);
+    }
+    if let Some(i) = matches.value_of("depth") {
+     //let depth = i.parse::<i32>().expect("depth is not a valid int");
+         println!("{:?}", i);
+    }
+
+    let tree: TreeProps = TreeProps {
+        angle_change: 20.0,
+        depth: 15,
+    };
+
+    with_2d_graphics( move || {
 
         let mut window_properties       = WindowProperties::from(&"Fractal Tree Generator");
-        window_properties.mouse_pointer = BindRef::from(bind(MousePointer::None));
+      //  window_properties.mouse_pointer = BindRef::from(bind(MousePointer::None));
         
         
         let (canvas, events) = create_canvas_window_with_events(window_properties);
@@ -25,18 +67,15 @@ fn main() {
 
         canvas.draw(|gc| {
             let SCR_HEIGHT = 100.0*DEPTH as f32;
-            gc.line_width(1.0);
             gc.stroke_color(Color::Rgba(1.0, 1.0, 1.0, 1.0));
-            gc.draw_rect();
-
-
+           // gc.draw_rect();
 
             gc.line_width(LINE_WIDTH);
-            gc.stroke_color(COLOR);
+            
             gc.canvas_height(SCR_HEIGHT);
             gc.center_region(0.0, 0., SCR_HEIGHT, SCR_HEIGHT);
             
-            draw_tree(gc, SCR_HEIGHT/1.5, SCR_HEIGHT/10.0, 0.0, DEPTH);
+            draw_tree(gc, SCR_HEIGHT/2.0, SCR_HEIGHT/10.0,  tree.depth, 0.0, tree.angle_change);
            
             ()
         });
@@ -44,22 +83,33 @@ fn main() {
     })
 }
 
+fn get_color(n: i32) -> Color {
+    if n%2 == 0 {
+       return Color::Rgba(1.0, 0.0, 1.0, 1.0);
+    } else if n% 7 == 0 {
+        return Color::Rgba(0.0, 1.0, 1.0, 1.0); 
+    } else if n%3 == 0 {
+        return Color::Rgba(0.0, 1.0, 1.0, 1.0);
+    }
+    
+    Color::Rgba(1.0, 1.0, 1.0, 1.0) 
+}
+
     /// Populates the branches of a tree. and draws tree
-    pub fn draw_tree(gc: &mut CanvasGraphicsContext, x : f32 ,y : f32, angle: f32, n: i32) {
-        let mut rng = thread_rng();
-        let color = Color::Rgba(rng.gen_range(0.1..=1.0), 0.0, rng.gen_range(0.1..=1.0), 1.0);
+    pub fn draw_tree(gc: &mut CanvasGraphicsContext, x : f32 ,y : f32, n: i32, angle: f32, angle_change:f32) {
+        let mut color = get_color(n);
 
         let x2 = x + angle.to_radians().sin() * n as f32 * 10.0;
         let y2 = y + angle.to_radians().cos() * n as f32 * 10.0;
 
-        let bpb = BezierPathBuilder::start(Branch{x: x, y: y});
-        bpb.line_to(Branch{x: x2, y: y2});
-        let path = bpb.build()
+        //let bpb = BezierPathBuilder::start(Branch{x: x, y: y});
+       // bpb.line_to(Branch{x: x2, y: y2});
+       // let path = bpb.build()
         //slower than having stroking just once but this allows customisation of branches
-        //gc.stroke_color(color);
         //gc.draw_bzier_curve();
 
         gc.new_path();
+        gc.stroke_color(color);
         gc.move_to(x, y);
         gc.line_to(x2 , y2);
         gc.stroke();
@@ -67,8 +117,8 @@ fn main() {
         if n == 0 {
           return;
          }
-        draw_tree(gc, x2, y2, angle - ANGLE_START, n-1);
-        draw_tree(gc, x2, y2, angle + ANGLE_START, n-1);
+        draw_tree(gc, x2, y2, n-1, angle - angle_change, angle_change);
+        draw_tree(gc, x2, y2, n-1, angle + angle_change, angle_change);
 }
 
 struct Branch  {
@@ -76,14 +126,4 @@ struct Branch  {
     y: f64,
 }
 
-impl Coordinate2D for Branch {
-    fn x(&self) -> f64 {
-        self.x
-    }
-    fn y(&self) -> f64 {
-        self.y
-    }
-    fn coords(&self) -> (f64, f64) {
-        (self.x, self.y)
-     }
-} 
+
